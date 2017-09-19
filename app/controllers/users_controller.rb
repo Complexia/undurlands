7ccf666a858cb2293bdcf014b709b37367789de8
s3399752 <s3399752@student.rmit.edu.au>
2017-09-19 14:33:22 +0600

    login ready

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to Undurlands!"
+      log_in @user      
       redirect_to @user
     else
       render 'new'
